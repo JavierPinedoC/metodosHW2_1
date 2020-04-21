667c390d625c296b407a519cbc6d373841988c44
JavierPC <A01551344@itesm.mx>
2020-04-20 19:22:26 -0500

    metodo convert and matrix done

--- markov.java
+++ markov.java
@@ -1,6 +1,55 @@
+import java.util.*;
+
 public class markov {
 
     public static void main(String[] args) {
+        Scanner sc;
+        sc = new Scanner(System.in);
+        // System.out.print("Ingresa el numero de estados: ");
+        int estados = sc.nextInt();
+
+        double decimal[][] = new double[estados][estados];
+        double numerador[][] = new double[estados][estados];
+        double denominador[][] = new double[estados][estados];
+
+        int iteraciones = sc.nextInt();
+    }
+
+    public static void matrix(double[][] decimal, double[][] numerador, double[][] denominador) {
+        Scanner sc;
+        sc = new Scanner(System.in);
+        for (int i = 0; i < numerador.length; i++) {
+            for (int j = 0; j < numerador.length; j++) {
+                numerador[i][j] = sc.nextInt();
+                denominador[i][j] = sc.nextInt();
+                decimal[i][j] = numerador[i][j] / denominador[i][j];
+            }
+        }
+    }
+
+    public static void trans(int iteraciones, double[][] decimal, double[][] numerador, double[][] denominador) {
         
     }
+
+    public static String convert(double n) {
+        double tolerance = 1.0E-6;
+        double h1=1; double h2=0;
+        double k1=0; double k2=1;
+        double b = n;
+        do {
+            double a = Math.floor(b);
+            double aux = h1; 
+            h1 = a*h1+h2; 
+            h2 = aux;
+            aux = k1; 
+            k1 = a*k1+k2; 
+            k2 = aux;
+            b = 1/(b-a);
+        } while (Math.abs(n-h1/k1) > n*tolerance);
+
+        String str = " "+(int)h1+"/"+(int)k1+" ";
+        
+        return str;
+    }
+
 }
